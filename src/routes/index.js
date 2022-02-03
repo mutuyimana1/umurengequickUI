8521e28dbe20e1f057591001cc062678acc3c273
Iyamuremye Benjamin <yamubbenjamin@gmail.com>
2022-02-03 12:12:57 +0200

    mentor feedbacks

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Routes, Route,useLocation} from "react-router-dom";
-import Home from "../views/home"
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "../views/home";
 import Services from "../views/services";
 import Header from "../components/header";
 import ContactUs from "../views/contactUs";
@@ -9,27 +9,21 @@ import Schedule from "../views/Schedule";
 
 import AllBookings from "../views/dashboard/userDashboard";
 
+const Index = () => {
+  const currentUrl = useLocation.pathname;
 
-
-const Index=()=>{
-const currentUrl=useLocation.pathname
-
-return(
-
-<Routes>
-<Route path='/home' element={<Home/>} />
-<Route path='/schedule' element={<Schedule/>} />
-<Route path='/date' element={<DateAndTime/>} />
-<Route path='/serivisi' element={<Services/>}/>
-{/* <Route path='/header' element={<Header/>}/> */}
-<Route path='/contactUs' element={<ContactUs/>}/>
-<Route path='/header' element={<Header/>}/>
-</Routes>
-
-
-<Route path='/userboard' element={<AllBookings/>} />
-
-</Routes> 
-)
-}
-export default Index
+  return (
+    <Routes>
+    <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/schedule" element={<Schedule />} />
+      <Route path="/date" element={<DateAndTime />} />
+      <Route path="/serivisi" element={<Services />} />
+      {/* <Route path='/header' element={<Header/>}/> */}
+      <Route path="/contactUs" element={<ContactUs />} />
+      <Route path="/header" element={<Header />} />
+      <Route path="/userboard" element={<AllBookings />} />
+    </Routes>
+  );
+};
+export default Index;
